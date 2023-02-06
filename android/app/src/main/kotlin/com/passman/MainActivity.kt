0e5e703d4f109ca85591731a268796e4dc5848b0
Manoj Kumar Rajavarapu <74780798+lucifer-bannu@users.noreply.github.com>
2023-02-06 12:28:19 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/passman/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.passman
+
+import io.flutter.embedding.android.FlutterActivity
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+import io.flutter.embedding.engine.FlutterEngine
+
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
